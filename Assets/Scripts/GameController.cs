e39c75d796d0c07ccaf4691d82bd0369d269326e
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2020-06-17 18:58:56 +0000

    Added the moving walls

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Transform wallSpawnLocation;
+    public GameObject[] wallVariants;
+    public float wallSpeed = 5f;
+    public float waitAfterWallSpawn = 5f;
+    public float startWait = 2f;
+
+    private bool playing = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnWalls());
+    }
+
+    private IEnumerator SpawnWalls()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (playing)
+        {
+            spawnWall(wallVariants[Random.Range(0, wallVariants.Length - 1)]);
+            yield return new WaitForSeconds(waitAfterWallSpawn);
+        }
+    }
+
+    private void spawnWall(GameObject wall)
+    {
+        GameObject newWall = Instantiate(wall, wallSpawnLocation);
+        newWall.GetComponent<MeshRenderer>().enabled = true;// Af einhverri ástæðu er slökkt á þessu þegar veggnum er instantiatað svo að þetta þarf til þess að objectinn sjáist
+    }
+
+    public void GameOver()
+    {
+        playing = false;
+        Time.timeScale = 0;
+        Debug.Log("Game Over");
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    [Header("Hole Location Settings")]
+    public int HoleHorizontalLoc;
+    public bool jumpRequired;
+    public bool crouchRequired;
+
+    private GameController gc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gc = GameObject.FindGameObjectWithTag("Game Controller").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!gc) return;
+        transform.position += new Vector3(0, 0, gc.wallSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Tag: " + other.tag);
+        if (other.tag == "Player")
+        {
+            PlayerScript player = other.GetComponent<PlayerScript>();
+
+            if (crouchRequired && !player.crouching) gc.GameOver();
+            else if (jumpRequired && !player.jumping) gc.GameOver();
+            else if (HoleHorizontalLoc != player.horizontalPos) gc.GameOver();
+        }
+        if (other.tag == "Wall Despawner") Destroy(other.gameObject);
+    }
+}
